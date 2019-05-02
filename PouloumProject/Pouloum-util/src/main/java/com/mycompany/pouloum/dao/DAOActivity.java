7d83127ae1e643f320ba645c29047907da08c4ad
tempteamcode <38981204+tempteamcode@users.noreply.github.com>
2019-05-02 11:21:19 +0200

    add DAOActivity

---
+++ PouloumProject/Pouloum-util/src/main/java/com/mycompany/pouloum/dao/DAOActivity.java
@@ -0,0 +1,78 @@
+package com.mycompany.pouloum.dao;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
+import com.mycompany.pouloum.model.Activity;
+
+public class DAOActivity {
+
+    public static void persist(Activity a)
+            throws Exception {
+        EntityManager em = JpaUtil.getEntityManager();
+
+        try {
+            em.persist(a);
+        } catch (Exception ex) {
+            throw ex;
+        }
+    }
+
+    public static void removeById(Long id)
+            throws Exception {
+        EntityManager em = JpaUtil.getEntityManager();
+
+        try {
+            Activity a = (Activity) em.find(Activity.class, id);
+            em.remove(a);
+        } catch (Exception ex) {
+            throw ex;
+        }
+    }
+
+    public static Activity updateUser(Activity u)
+            throws Exception {
+        EntityManager em = JpaUtil.getEntityManager();
+
+        try {
+            em.merge(u);
+        } catch (Exception ex) {
+            throw ex;
+        }
+
+        return u;
+    }
+
+    public static Activity findById(Long id)
+            throws Exception {
+        EntityManager em = JpaUtil.getEntityManager();
+
+        Activity found = null;
+
+        try {
+            found = em.find(Activity.class, id);
+        } catch (Exception ex) {
+            throw ex;
+        }
+
+        return found;
+    }
+
+    public static List<Activity> findAll()
+            throws Exception {
+        EntityManager em = JpaUtil.getEntityManager();
+
+        List<Activity> found = null;
+
+        try {
+            Query q = em.createQuery("SELECT a FROM Activity a");
+            found = (List<Activity>) q.getResultList();
+        } catch (Exception ex) {
+            throw ex;
+        }
+
+        return found;
+    }
+
+}
