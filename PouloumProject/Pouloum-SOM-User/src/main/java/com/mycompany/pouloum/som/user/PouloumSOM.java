1c6049cb4ebb11980f881590f54ae17e492d1fd5
FrancoisMontigny <francois.montigny@gmail.com>
2019-05-02 10:45:45 +0200

    feat: added support for connection using either nickname or mail

--- PouloumProject/Pouloum-SOM-User/src/main/java/com/mycompany/pouloum/som/user/PouloumSOM.java
+++ PouloumProject/Pouloum-SOM-User/src/main/java/com/mycompany/pouloum/som/user/PouloumSOM.java
@@ -7,23 +7,62 @@ package com.mycompany.pouloum.som.user;
 
 import com.google.gson.JsonObject;
 import com.mycompany.pouloum.util.DBConnection;
+import com.mycompany.pouloum.dao.JpaUtil;
+import com.mycompany.pouloum.dao.DAOUser;
+import com.mycompany.pouloum.model.User;
 
 /**
  *
  * @author Martin
  */
 public class PouloumSOM {
-    
+
     protected DBConnection dBConnection;
     protected JsonObject container;
-    
+
     public PouloumSOM(DBConnection dBConnection, JsonObject container) {
         this.dBConnection = dBConnection;
         this.container = container;
     }
-    
+
     public void release() {
         this.dBConnection.close();
     }
+
+    /**
+     * Try to login with a given (mail, password) pair.
+     *
+     * @param mail is the mail used to login.
+     * @param password is the password of the account.
+     * @return User, the user matching the credentials or null if they are
+     * incorrect.
+     */
+    public User login(String mail, String password) {
+        JpaUtil.createEntityManager();
+
+        User u = DAOUser.findUserByEmailAndPassword(mail, password);
+
+        JpaUtil.closeEntityManager();
+
+        return u;
+    }
     
+    /**
+     * Try to login with a given (nickname, password) pair.
+     *
+     * @param nickname is the nickname used to login.
+     * @param password is the password of the account.
+     * @return User, the user matching the credentials or null if they are
+     * incorrect.
+     */
+    public User login(String nickname, String password) {
+        JpaUtil.createEntityManager();
+
+        User u = DAOUser.findUserByNicknameAndPassword(nickname, password);
+
+        JpaUtil.closeEntityManager();
+
+        return u;
+    }
+
 }
