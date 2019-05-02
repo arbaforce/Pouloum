4b92e11ca752b33d8725b9cf1662ebab5ab3f9d6
Kragzmo-Simon <fery.simon@ymail.com>
2019-05-02 15:03:12 +0200

    SOM Event WIP

--- PouloumProject/Pouloum-SOM-Event/src/main/java/com/mycompany/pouloum/som/event/PouloumSOM.java
+++ PouloumProject/Pouloum-SOM-Event/src/main/java/com/mycompany/pouloum/som/event/PouloumSOM.java
@@ -7,6 +7,11 @@ package com.mycompany.pouloum.som.event;
 
 import com.google.gson.JsonObject;
 import com.mycompany.pouloum.util.DBConnection;
+import com.mycompany.pouloum.model.Event;
+import com.mycompany.pouloum.dao.JpaUtil;
+import com.mycompany.pouloum.dao.DAOEvent;
+import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -25,4 +30,37 @@ public class PouloumSOM {
         this.dBConnection.close();
     }
   
+    /**
+     * Try to login with a given (nickname, password) pair.
+     *
+     * @param id is the event id.
+     * @return Event, the event matching to the id.
+     * @throws Exception if there's an error trying to access the database.
+     */
+    public Event getEventById(Long id) 
+            throws Exception 
+    {
+        JpaUtil.createEntityManager();
+        
+        Event e = DAOEvent.findById(id);
+        
+        JpaUtil.closeEntityManager();
+        
+        return e;
+    }
+    
+    /**
+     * Try to login with a given (nickname, password) pair.
+     *
+     * @param interests, the list containing all activities of the search.
+     * @return EventList, a list containing all events and for each event, 
+     * the ids of the participants.
+     * @throws Exception if there's an error trying to access the database.
+     */
+    public Map<Event,List<Long>> getEventByInterests(List<Long> interests) 
+            throws Exception
+    {
+        // do magical stuff plz
+        return null;
+    }    
 }
