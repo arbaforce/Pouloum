c62e14ea24e7b3b90355d6babc1c022745a33180
ratatouille <7spherebleue7@gmail.com>
2019-05-03 10:41:14 +0200

    map bar : fix-it

--- PouloumProject/Pouloum-IHM-WEB/src/main/webapp/SCRIPT/events_browser.js
+++ PouloumProject/Pouloum-IHM-WEB/src/main/webapp/SCRIPT/events_browser.js
@@ -4,13 +4,13 @@
  * and open the template in the editor.
  */
 function openNav() {
-  document.getElementById("search_result_map_global_container").style.width = "50%";
+  document.getElementById("search_result_map_global_col").style.width = "50%";
   document.getElementById("search_result_list_global_container").style.marginRight = "700px";
   document.getElementById("map_Button").setAttribute("onclick", "closeNav()");
 }
 
 function closeNav() {
-  document.getElementById("search_result_map_global_container").style.width = "65px";
+  document.getElementById("search_result_map_global_col").style.width = "65px";
   document.getElementById("search_result_list_global_container").style.marginRight= "65px";
   document.getElementById("map_Button").setAttribute("onclick", "openNav()");
 }
