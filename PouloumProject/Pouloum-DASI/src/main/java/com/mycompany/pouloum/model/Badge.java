2cd7bdc4ee183102f72ff16d5edea0d35a07f228
tempteamcode <38981204+tempteamcode@users.noreply.github.com>
2019-05-03 10:23:02 +0200

    improve Badge toJson

--- PouloumProject/Pouloum-DASI/src/main/java/com/mycompany/pouloum/model/Badge.java
+++ PouloumProject/Pouloum-DASI/src/main/java/com/mycompany/pouloum/model/Badge.java
@@ -6,21 +6,11 @@ public enum Badge {
     FIRE,
     WATER,
     GRASS;
-
+    
     public JsonObject toJson() {
         JsonObject obj = new JsonObject();
         
-        switch(this) {
-            case FIRE:
-                obj.addProperty("type", "fire");
-                break;
-            case WATER:
-                obj.addProperty("type", "water");
-                break;
-            case GRASS:
-                obj.addProperty("type", "grass");
-                break;
-        }
+        obj.addProperty("type", this.toString());
         
         return obj;        
     }
