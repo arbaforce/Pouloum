cdad0f2317614cd7af7e5b172b54eedfe2382612
tempteamcode <38981204+tempteamcode@users.noreply.github.com>
2019-05-06 11:54:38 +0200

    add dynamic events part 6

--- PouloumProject/Pouloum-IHM-WEB/src/main/webapp/SCRIPT/events_browser.js
+++ PouloumProject/Pouloum-IHM-WEB/src/main/webapp/SCRIPT/events_browser.js
@@ -74,6 +74,7 @@ function degresMinutesToDecimal(longDeg, longMin, longDir, latDeg, latMin, latDi
 }
 
 function addResultDemo() {
+    data_badges = ["Wind", "Fire"];
     
     data_participants = [];
     for (var i=0; i<14; i++) {
@@ -84,10 +85,10 @@ function addResultDemo() {
         });
     }
     
-    addResult([{name:"Basketball", url:"", level:"débutant"}], {name:"Claude", url:""}, "Tête d'Or", "aujourd'hui", "une semaine", data_participants, "rempli", "");
+    addResult([{name:"Basketball", url:"", level:"débutant"}], {name:"Claude", url:""}, "Tête d'Or", "aujourd'hui", "une semaine", data_participants, "rempli", "", data_badges);
 }
 
-function addResult(data_activities, data_organizer, data_place, data_date, data_duration, data_participants, data_status, data_eventurl) {
+function addResult(data_activities, data_organizer, data_place, data_date, data_duration, data_participants, data_status, data_eventurl, data_badges) {
     var results = document.getElementById("search_result_list_global_container");
     
     var div_event_container = document.createElement("div");
@@ -356,28 +357,52 @@ function addResult(data_activities, data_organizer, data_place, data_date, data_
                     
                     div_event_status_know_more_no_more_button_badges_row.appendChild(div_event_statut_know_more_col);
                     
-                    //TODO
-                    
-                    /*
-                    </div><!--#event_status_know_more_col -->
-                    <div id="event_no_more_button_col" class="col mx-0 px-0">
-                        <button id="buttonLeaveEvent" type="button" class="btn btn-light buttonEvent" >
-                            Je ne participe plus.
-                        </button><!--#buttonLeaveEvent -->
-                    </div><!--#event_no_more_button_col -->
-                    <div id="event_badges_col" class="col mx-0 px-0" >
-                        
-                        <div id="event_badges_container" class="container" >
+                    var div_event_no_more_button_col = document.createElement("div");
+                    div_event_no_more_button_col.className = "col mx-0 px-0";
+                    {
+                        var button_knowmore = document.createElement("button");
+                        button_knowmore.className = "btn btn-light buttonEvent";
+                        button_knowmore.type = "button";
+                        {
+                            var txt_knowmore = document.createTextNode("Je ne participe plus.");
                             
-                            <div id="event_badges_row" class="row mx-0 event_badges_row">
-                                <img src="STYLE/Wind.png" alt="image" class="little_avatar_image rounded " /> 
-                                <img src="STYLE/Fire.png" alt="image" class="little_avatar_image rounded " /> 
-                            </div><!--#event_badges_row -->
-                        </div><!--#event_badges_container -->
+                            button_knowmore.appendChild(txt_knowmore);
+                        }
                         
-                    </div><!--#event_badges_col -->
-                    */
+                        div_event_no_more_button_col.appendChild(button_knowmore);
+                    }
                     
+                    div_event_status_know_more_no_more_button_badges_row.appendChild(div_event_no_more_button_col);
+                    
+                    var div_event_badges_col = document.createElement("div");
+                    div_event_badges_col.className = "col mx-0 px-0";
+                    {
+                        var div_event_badges_container = document.createElement("div");
+                        div_event_status_container.className = "container";
+                        {
+                            var div_event_badges_row = document.createElement("div");
+                            div_event_badges_row.className = "row mx-0 event_badges_row";
+                            {
+                                for (var i=0; i<data_badges.length; i++)
+                                {
+                                    var data_badge = data_badges[i];
+                                    
+                                    var img_badge = document.createElement("img");
+                                    img_badge.className = "little_avatar_image rounded";
+                                    img_badge.src = "STYLE/" + data_badge + ".png";
+                                    img_badge.alt = data_badge;
+                                    
+                                    div_event_badges_row.appendChild(img_badge);
+                                }
+                            }
+                            
+                            div_event_badges_container.appendChild(div_event_badges_row);
+                        }
+                        
+                        div_event_badges_col.appendChild(div_event_badges_container);
+                    }
+                    
+                    div_event_status_know_more_no_more_button_badges_row.appendChild(div_event_badges_col);
                 }
                 
                 div_event_status_know_more_no_more_button_badges_container.appendChild(div_event_status_know_more_no_more_button_badges_row);
