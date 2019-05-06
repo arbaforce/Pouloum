91d1304d3464463a09a1b0540062285770d90cb0
FrancoisMontigny <francois.montigny@gmail.com>
2019-05-06 15:01:29 +0200

    feat: added a check for overlapping conflicts when trying to join an event

--- PouloumProject/Pouloum-DASI/src/main/java/com/mycompany/pouloum/service/ServicesServlet.java
+++ PouloumProject/Pouloum-DASI/src/main/java/com/mycompany/pouloum/service/ServicesServlet.java
@@ -331,10 +331,20 @@ public class ServicesServlet extends HttpServlet {
                 Pouloumer p = ServicesPouloumer.getPouloumerById(idUser);
                 Event e = ServicesEvent.getEventById(idEvent);
 
-                // the GUI should check that the event is not full (so an user
-                // never join a full event)
-                //TODO check that user doesn't have other events at the same time
-                ServicesPouloumer.joinEvent(p, e);
+                CRE checkResult = ServicesPouloumer.checkForOverlapsingEvents(p, e);
+
+                if (checkResult == CRE_ERR_EVENT_AT_SAME_TIME) {
+                    resultErrorMessage = "The user is already participating to an event happening at the same time.";
+                } else {
+                    if (checkResult == CRE_WAR_EVENT_NEAR) {
+                        //TODO notify user
+                    }
+
+                    // the GUI should check that the event is not full (so an user
+                    // never join a full event)
+                    //TODO check that user doesn't have other events at the same time
+                    ServicesPouloumer.joinEvent(p, e);
+                }
             } ///////////
             ////Leave event
             ///////////
