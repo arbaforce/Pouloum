0be24bf18a9855730a3f360c9932b132eff08546
jeannib <bast.jeannin@gmail.com>
2019-05-06 09:12:02 +0200

    navbar_template iuntegrated to the whole project.

---
+++ PouloumProject/Pouloum-IHM-WEB/src/main/webapp/SCRIPT/navbar_template.js
@@ -0,0 +1,95 @@
+var csscode =  `
+
+.newnavbar {
+    display:flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    align-items: center;
+    
+    background-color: #f8f9fa;
+}
+
+.newnavbar a {
+    color: black;
+}
+
+`;
+
+var htmlcode = `
+            <div class="newnavbar" id="navbar">
+                <div id="navbar_brand_column" class="col-2 h-100">
+                    <a class="navbar-brand text-center" href="home_page.html">
+                        P <!-- TODO replace by company logo -->
+                    </a>
+                </div> <!--#navbar_brand_column-->
+                <div>
+                    <a class="nav-link font-weight-bold text-center" href="home_page.html">
+                        Accueil
+                    </a>
+                </div>
+                <div>
+                    <a class="nav-link dropdown-toggle font-weight-bold text-center" href="#" id="navbar_profile_dropdown_links" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Profil
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbar_profile_dropdown_links">
+                        <a class="dropdown-item font-weight-bold text-center" href="my_profile.html">
+                            Mon Profil
+                        </a>
+                        <a class="dropdown-item font-weight-bold text-center" href="profile_history.html">
+                            Historique
+                        </a>
+                        <a class="dropdown-item font-weight-bold text-center" href="profile_relations.html">
+                            Relations  
+                        </a>
+                    </div>
+                </div>
+                <div>
+                    <a class="nav-link font-weight-bold text-center" href="events_browser.html">
+                        Chercher
+                    </a>
+                </div>
+                <div>
+                    <a class="nav-link dropdown-toggle font-weight-bold text-center" href="#" id="navbar_organize_dropdown_links" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Organiser
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbar_organize_dropdown_links">
+                        <a class="dropdown-item font-weight-bold text-center" href="modify_event.html?create=1&idEvent=null">
+                            Nouvel Evènement
+                        </a>
+                        <a class="dropdown-item font-weight-bold text-center" href="modify_event.html?create=0">
+                            Modifier
+                        </a>
+                    </div>
+                </div>
+                <div>
+                    <a class="nav-link font-weight-bold text-center" href="activities_browser.html">
+                        Activité
+                    </a>
+                </div>
+                <div>
+                    <div class="nav-link font-weight-bold text-center" href="index.html">
+                        <img src="IMG/navbar_logout_icon.png" style="max-width: 1.5em; max-height: 1.5em;">
+                    </div>
+                </div>
+            </div> <!--#navbar-->
+`;
+
+
+
+var node = document.currentScript;
+var parent = node.parentNode;
+
+var css = document.createElement('style');
+css.innerHTML = csscode;
+parent.insertBefore(css, node);
+
+var html = document.createElement('div');
+html.innerHTML = htmlcode;
+parent.insertBefore(html, node);
+
+for (var innerElem; innerElem = html.firstChild;) {
+    parent.insertBefore(innerElem, html);
+}
+parent.removeChild(html);
+
+parent.removeChild(node);
