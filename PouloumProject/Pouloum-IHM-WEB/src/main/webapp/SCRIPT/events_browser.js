2249f136ead8f97c9bf05babecf79c2adeb7ccd2
tempteamcode <38981204+tempteamcode@users.noreply.github.com>
2019-05-06 11:34:14 +0200

    add dynamic events part 5

--- PouloumProject/Pouloum-IHM-WEB/src/main/webapp/SCRIPT/events_browser.js
+++ PouloumProject/Pouloum-IHM-WEB/src/main/webapp/SCRIPT/events_browser.js
@@ -84,10 +84,10 @@ function addResultDemo() {
         });
     }
     
-    addResult([{name:"Basketball", url:"", level:"débutant"}], {name:"Claude", url:""}, "Tête d'Or", "aujourd'hui", "une semaine", data_participants);
+    addResult([{name:"Basketball", url:"", level:"débutant"}], {name:"Claude", url:""}, "Tête d'Or", "aujourd'hui", "une semaine", data_participants, "rempli", "");
 }
 
-function addResult(data_activities, data_organizer, data_place, data_date, data_duration, data_participants) {
+function addResult(data_activities, data_organizer, data_place, data_date, data_duration, data_participants, data_status, data_eventurl) {
     var results = document.getElementById("search_result_list_global_container");
     
     var div_event_container = document.createElement("div");
@@ -198,15 +198,15 @@ function addResult(data_activities, data_organizer, data_place, data_date, data_
                         var div_users_title_container = document.createElement("div");
                         div_users_title_container.className = "container h-100 w-100";
                         {
-		                    var div_users_title_row = document.createElement("div");
-		                    div_users_title_row.className = "row mx-0 px-0 h-100 w-100";
-		                    {
-		                        var txt_participants = document.createTextNode("Participant(s) : ");
-		                        
-		                        div_users_title_row.appendChild(txt_participants);
-		                    }
-		                    
-		                    div_users_title_container.appendChild(div_users_title_row);
+                            var div_users_title_row = document.createElement("div");
+                            div_users_title_row.className = "row mx-0 px-0 h-100 w-100";
+                            {
+                                var txt_participants = document.createTextNode("Participant(s) : ");
+                                
+                                div_users_title_row.appendChild(txt_participants);
+                            }
+                            
+                            div_users_title_container.appendChild(div_users_title_row);
                         }
                         
                         div_users_col.appendChild(div_users_title_container);
@@ -214,9 +214,9 @@ function addResult(data_activities, data_organizer, data_place, data_date, data_
                         var div_users_container = document.createElement("div");
                         div_users_container.className = "container";
                         {
-		                    var div_users_row = document.createElement("div");
-		                    div_users_row.className = "row mx-0 px-0 h-100 w-100 users_row";
-		                    {
+                            var div_users_row = document.createElement("div");
+                            div_users_row.className = "row mx-0 px-0 h-100 w-100 users_row";
+                            {
                                 for (var i=0; i<data_participants.length; i++)
                                 {
                                     var data_participant = data_participants[i];
@@ -234,9 +234,9 @@ function addResult(data_activities, data_organizer, data_place, data_date, data_
                                     
                                     div_users_row.appendChild(a_participant);
                                 }
-		                    }
-		                    
-		                    div_users_container.appendChild(div_users_row);
+                            }
+                            
+                            div_users_container.appendChild(div_users_row);
                         }
                         
                         div_users_col.appendChild(div_users_container);
@@ -271,7 +271,7 @@ function addResult(data_activities, data_organizer, data_place, data_date, data_
                     div_event_date_row.appendChild(div_event_date_col);
                 }
                 
-                div_event_place_container.appendChild(div_event_date_row);
+                div_event_date_container.appendChild(div_event_date_row);
             }
             
             div_event_row.appendChild(div_event_date_container);
@@ -290,7 +290,7 @@ function addResult(data_activities, data_organizer, data_place, data_date, data_
                         div_event_time_col.appendChild(txt_time);
                         
                         var txt_time = document.createTextNode(data_duration);
-                            
+                        
                         div_event_time_col.appendChild(txt_time);
                     }
                     
@@ -302,21 +302,63 @@ function addResult(data_activities, data_organizer, data_place, data_date, data_
             
             div_event_row.appendChild(div_event_time_container);
             
-            /*
-            <div id="event_status_know_more_no_more_button_badges_container" class="container event_status_know_more_no_more_button_badges_container">
-                <div id="event_status_know_more_no_more_button_badges_row" class="row mx-0 px-0">
+            var div_event_status_know_more_no_more_button_badges_container = document.createElement("div");
+            div_event_status_know_more_no_more_button_badges_container.className = "container event_status_know_more_no_more_button_badges_container";
+            {
+                var div_event_status_know_more_no_more_button_badges_row = document.createElement("div");
+                div_event_status_know_more_no_more_button_badges_row.className = "row mx-0 px-0";
+                {
+                    var div_event_statut_know_more_col = document.createElement("div");
+                    div_event_statut_know_more_col.className = "col-6 event_statut_know_more_col mx-0 px-0";
+                    {
+                        var div_event_status_container = document.createElement("div");
+                        div_event_status_container.className = "container";
+                        {
+                            var div_event_status_row = document.createElement("div");
+                            div_event_status_row.className = "row mx-0 px-0";
+                            {
+                                var txt_statuses = document.createTextNode("Statut : ");
+                                
+                                div_event_status_row.appendChild(txt_statuses);
+                                
+                                var txt_status = document.createTextNode(data_status + ".");
+                                
+                                div_event_status_row.appendChild(txt_status);
+                            }
+                            
+                            div_event_status_container.appendChild(div_event_status_row);
+                            
+                            var div_event_know_more_container = document.createElement("div");
+                            div_event_know_more_container.className = "container";
+                            {
+                                var div_event_know_more_row = document.createElement("div");
+                                div_event_know_more_row.className = "row mx-0 px-0";
+                                {
+                                    var a_knowmore = document.createElement("a");
+                                    a_knowmore.href = data_eventurl;
+                                    {
+                                        var txt_knowmore = document.createTextNode("En savoir plus");
+                                        
+                                        a_knowmore.appendChild(txt_knowmore);
+                                    }
+                                    
+                                    div_event_know_more_row.appendChild(a_knowmore);
+                                }
+                                
+                                div_event_know_more_container.appendChild(div_event_know_more_row);
+                            }
+                            
+                            div_event_status_container.appendChild(div_event_know_more_container);
+                        }
+                        
+                        div_event_statut_know_more_col.appendChild(div_event_status_container);
+                    }
                     
-                    <div id="event_statut_know_more_col" class="col-6 event_statut_know_more_col mx-0 px-0">
-                        <div id="event_status_container" class="container">
-                            <div id="event_status_row" class="row mx-0 px-0">
-                                Statut : remplie.
-                            </div>  <!--#event_status_row -->
-                        </div><!--#event_status_container -->
-                        <div id="event_know_more_container" class="container">
-                            <div id="event_know_more_row" class="row mx-0 px-0">
-                                <a href="">En savoir plus</a>
-                            </div>  <!--#event_know_more_row -->
-                        </div><!--#event_know_more_container -->
+                    div_event_status_know_more_no_more_button_badges_row.appendChild(div_event_statut_know_more_col);
+                    
+                    //TODO
+                    
+                    /*
                     </div><!--#event_status_know_more_col -->
                     <div id="event_no_more_button_col" class="col mx-0 px-0">
                         <button id="buttonLeaveEvent" type="button" class="btn btn-light buttonEvent" >
@@ -332,15 +374,16 @@ function addResult(data_activities, data_organizer, data_place, data_date, data_
                                 <img src="STYLE/Fire.png" alt="image" class="little_avatar_image rounded " /> 
                             </div><!--#event_badges_row -->
                         </div><!--#event_badges_container -->
-                                
-                    </div><!--#event_badges_col -->
                         
+                    </div><!--#event_badges_col -->
+                    */
                     
-                    
-                    
-                </div><!--#event_status_know_more_no_more_button_badges_row -->
-            </div><!--#event_status_know_more_no_more_button_badges_container -->
-            */
+                }
+                
+                div_event_status_know_more_no_more_button_badges_container.appendChild(div_event_status_know_more_no_more_button_badges_row);
+            }
+            
+            div_event_row.appendChild(div_event_status_know_more_no_more_button_badges_container);
         }
         
         div_event_container.appendChild(div_event_row);
