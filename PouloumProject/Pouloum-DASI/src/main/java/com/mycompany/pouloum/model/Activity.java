a068b37a5c10905a4950461700d2690f63d188f7
FrancoisMontigny <francois.montigny@gmail.com>
2019-05-06 09:43:10 +0200

    feat: added script in activity_details

--- PouloumProject/Pouloum-DASI/src/main/java/com/mycompany/pouloum/model/Activity.java
+++ PouloumProject/Pouloum-DASI/src/main/java/com/mycompany/pouloum/model/Activity.java
@@ -11,41 +11,38 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
-
 @Entity
-public class Activity implements Serializable  {
-    
+public class Activity implements Serializable {
+
     // ATTRIBUTES
-    
     // Identifier
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     protected Long id;
-    
+
     // Hierarchy
     @OneToOne
     protected Activity parent;
     @OneToMany
     protected List<Activity> children;
-    
+
     // Description
     protected String name;
     protected String description;
     protected List<Badge> badges;
-    
+
     // Links
     protected String rules;
     protected List<String> resources;
-    
+
     // Default data
     protected int default_participants_min;
     protected int default_participants_max;
-    
-    
+
     // CONSTRUCTORS
-    
-    public Activity( ) {}
-    
+    public Activity() {
+    }
+
     public Activity(Activity parent, List<Activity> children, String name, String description, List<Badge> badges, String rules, List<String> resources, int default_participants_min, int default_participants_max) {
         this.parent = parent;
         this.children = children;
@@ -57,15 +54,13 @@ public class Activity implements Serializable  {
         this.default_participants_min = default_participants_min;
         this.default_participants_max = default_participants_max;
     }
-    
-    
+
     // GETTERS AND SETTERS
-    
-    public Long getId( ) {
+    public Long getId() {
         return id;
     }
-    
-    public void setId( Long id ) {
+
+    public void setId(Long id) {
         this.id = id;
     }
 
@@ -128,7 +123,7 @@ public class Activity implements Serializable  {
     public int getDefault_participants_min() {
         return default_participants_min;
     }
-    
+
     public void setDefault_participants_min(int default_participants_min) {
         this.default_participants_min = default_participants_min;
     }
@@ -136,46 +131,46 @@ public class Activity implements Serializable  {
     public int getDefault_participants_max() {
         return default_participants_max;
     }
-    
+
     public void setDefault_participants_max(int default_participants_max) {
         this.default_participants_max = default_participants_max;
     }
-    
+
     public JsonObject toJson() {
         JsonObject obj = new JsonObject();
-        
+
         obj.addProperty("id", id);
         obj.add("parent", parent.toJson());
-        
+
         JsonArray childrenArray = new JsonArray();
-        
+
         for (Activity a : children) {
             childrenArray.add(a.toJson());
         }
-        
+
         obj.add("children", childrenArray);
         obj.addProperty("name", name);
         obj.addProperty("description", description);
-        
+
         JsonArray badgesArray = new JsonArray();
-        
+
         for (Badge b : badges) {
             badgesArray.add(b.toJson());
         }
-        
+
         obj.add("badges", badgesArray);
         obj.addProperty("rules", rules);
-        
+
         JsonArray resourcesArray = new JsonArray();
-        
+
         for (String s : resources) {
             resourcesArray.add(s);
         }
-        
+
         obj.add("resources", resourcesArray);
-        obj.addProperty("min_participants", default_participants_min);
-        obj.addProperty("max_participants", default_participants_max);
-        
+        obj.addProperty("default_participants_min", default_participants_min);
+        obj.addProperty("default_participants_max", default_participants_max);
+
         return obj;
     }
 }
