99c4d2c1e317ceacf6cfac177597cd25c0a0d3ce
tempteamcode <38981204+tempteamcode@users.noreply.github.com>
2019-05-07 09:58:51 +0200

    fix SetupDB.setupEvents

--- PouloumProject/Pouloum-DASI/src/main/java/com/mycompany/pouloum/setup/SetupDB.java
+++ PouloumProject/Pouloum-DASI/src/main/java/com/mycompany/pouloum/setup/SetupDB.java
@@ -143,29 +143,90 @@ public class SetupDB {
             }
         }
     }
-
+    
     protected static void setupEvents()
             throws Exception {
         List<Activity> activities = ServicesActivity.findAllActivities();
         List<Event> events = new ArrayList<Event>();
+        
+        
+        List<Pouloumer> users = new ArrayList<>();
+        
+        Date d1 = DateUtil.toDate("16/08/1984");
+        Address a1 = new Address("6", "Rue Camille Koechlin", "Villeurbanne", "69100", "France");
+        Pouloumer p_Momo = new Pouloumer("Momo", "Moez", "WOAGNER", "moez.woagner@hotmail.fr", "mdpmw", false, false, 'M', d1,"0832205629", a1);
+        users.add(p_Momo);
+        
+        Date d2 = DateUtil.toDate("17/02/1996");
+        Address a2 = new Address("9", "Impasse Guillet", "Villeurbanne", "69100", "France");
+        Pouloumer p_Matty = new Pouloumer("Matty", "Matteo", "HONRY", "matteo.honry@hotmail.fr", "mdpmh", false, false, 'M', d2,"0482381862", a1);
+        users.add(p_Matty);
+        
+        Date d3 = DateUtil.toDate("16/02/1982");
+        Address a3 = new Address("20", "Rue Decomberousse", "Villeurbanne", "69100", "France");
+        Pouloumer p_Keke = new Pouloumer("Keke", "Kevin", "CECCANI", "kevin.ceccani@hotmail.fr", "mdpkc", false, false, 'M', d3,"0664426037", a3);
+        users.add(p_Keke);
+        
+        Date d4 = DateUtil.toDate("13/08/1988");
+        Address a4 = new Address("1", "Rue d'Alsace", "Villeurbanne", "69100", "France");
+        Pouloumer p_Valice = new Pouloumer("Valice", "Alice", "VOYRET", "alice.voyret@hotmail.fr", "mdpav", false, false, 'F', d4,"0486856520", a4);
+        users.add(p_Valice);
+        
+        Date d5 = DateUtil.toDate("16/05/1989");
+        Address a5 = new Address("4", "Rue de la Jeunesse", "Villeurbanne", "69100", "France");
+        Pouloumer p_Juju = new Pouloumer("Juju", "Julien", "RINERD", "jrinerd5241@hotmail.fr", "mdpjr", false, false, 'M', d5,"0727252485", a5);
+        users.add(p_Juju);
+        
+        Date d6 = DateUtil.toDate("24/05/1983");
+        Address a6 = new Address("7", "Rue de la Cloche", "Villeurbanne", "69100", "France");
+        Pouloumer p_Olive = new Pouloumer("Olive", "Olivier", "WOSTPHOL", "owostphol@hotmail.fr", "mdpow", false, false, 'M', d6,"0860680312", a6);
+        users.add(p_Olive);
+        
+        JpaUtil.createEntityManager();
+        
+        try {
+            for (Pouloumer u : users) {
+                try {
+                    Pouloumer check;
+                    check = DAOPouloumer.findPouloumerByEmail(u.getEmail());
+                    if (check != null) continue;
+                    check = DAOPouloumer.findPouloumerByNickname(u.getNickname());
+                    if (check != null) continue;
+                    
+                    JpaUtil.openTransaction();
+                    
+                    try {
+                        DAOPouloumer.persist(u);
+                        
+                        JpaUtil.commitTransaction();
+                    } catch (Exception ex) {
+                        JpaUtil.cancelTransaction();
+                        throw ex;
+                    }
+                    
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+            
+        } finally {
+            JpaUtil.closeEntityManager();
+        }
+        
+        Pouloumer p1 = p_Momo;
+        Date d01 = new Date();
+        Event e1 = new Event("1er evenement", "such description", d01, false, 30, a1, activities.get(0), p1, 1, 10);
+        Event e2 = new Event("2er evenement", "wow description", d01, false, 60, a1, activities.get(1), p1, 3000, 9000);
+        Event e3 = new Event("3eme evenement", "damn description", d01, false, 80, a1, activities.get(2), p1, 2, 4);
 
-        Pouloumer p1 = ServicesPouloumer.getPouloumerByNickname("Momo");
-        Address a1 = p1.getAddress();
-        Date d1 = new Date();
-        Event e1 = new Event("1er evenement", "such description", d1, false, 30, a1, activities.get(0), p1, 1, 10);
-        Event e2 = new Event("2er evenement", "wow description", d1, false, 60, a1, activities.get(1), p1, 3000, 9000);
-        Event e3 = new Event("3eme evenement", "damn description", d1, false, 80, a1, activities.get(2), p1, 2, 4);
+        Pouloumer p2 = p_Keke;
+        Date d02 = new Date();
+        Event e4 = new Event("my keke", "such kek", d02, false, 70, a2, activities.get(4), p2, 1, 10);
+        Event e5 = new Event("your keke", "dat kek", d02, false, 40, a2, activities.get(5), p2, 0, 15);
 
-        Pouloumer p2 = ServicesPouloumer.getPouloumerByNickname("Keke");
-        Address a2 = p1.getAddress();
-        Date d2 = new Date();
-        Event e4 = new Event("my keke", "such kek", d2, false, 70, a2, activities.get(4), p2, 1, 10);
-        Event e5 = new Event("your keke", "dat kek", d2, false, 40, a2, activities.get(5), p2, 0, 15);
-
-        Pouloumer p3 = ServicesPouloumer.getPouloumerByNickname("Matty");
-        Address a3 = p1.getAddress();
-        Date d3 = new Date();
-        Event e6 = new Event("hehehe", "useless information is useless", d3, false, 150, a3, activities.get(6), p3, 10, 12);
+        Pouloumer p3 = p_Matty;
+        Date d03 = new Date();
+        Event e6 = new Event("hehehe", "useless information is useless", d03, false, 150, a3, activities.get(6), p3, 10, 12);
 
         events.add(e1);
         events.add(e2);
@@ -174,18 +235,17 @@ public class SetupDB {
         events.add(e5);
         events.add(e6);
 
-        Pouloumer p4 = ServicesPouloumer.getPouloumerByNickname("Valice");
-        Pouloumer p5 = ServicesPouloumer.getPouloumerByNickname("Juju");
-        Pouloumer p6 = ServicesPouloumer.getPouloumerByNickname("Olive");
-
+        Pouloumer p4 = p_Valice;
+        Pouloumer p5 = p_Juju;
+        Pouloumer p6 = p_Olive;
+        
         JpaUtil.createEntityManager();
 
         try {
             for (Event e : events) {
                 try {
-
                     JpaUtil.openTransaction();
-
+                    
                     try {
                         DAOEvent.persist(e);
 
@@ -194,12 +254,10 @@ public class SetupDB {
                         JpaUtil.cancelTransaction();
                         throw ex;
                     }
-
                 } catch (Exception ex) {
                     ex.printStackTrace();
                 }
             }
-
         } finally {
             JpaUtil.closeEntityManager();
         }
