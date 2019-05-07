9421e35ade571d2886226e04127195aefeabfb60
ratatouille <7spherebleue7@gmail.com>
2019-05-07 08:19:49 +0200

    add map.js

--- PouloumProject/Pouloum-IHM-WEB/src/main/webapp/SCRIPT/events_browser.js
+++ PouloumProject/Pouloum-IHM-WEB/src/main/webapp/SCRIPT/events_browser.js
@@ -30,46 +30,6 @@ function closeNav() {
       events_status_know_more_no_more_button_badges_container[i].style.marginRight= "0px";
   }
   
-  
   document.getElementById("map_Button").setAttribute("onclick", "openNav()");
-}
-
-// source : https://leafletjs.com/examples/quick-start/
-function initialiseMap() {
-	map = L.map('map');
-	map.setView([45.74,4.84], 13);
-	var basemap = L.tileLayer('http://{s}.tile.stamen.com/terrain/{z}/{x}/{y}.png', {
-	        attribution: '<a href="http://content.stamen.com/dotspotting_toner_cartography_available_for_download">Stamen Toner</a>, <a href="http://www.openstreetmap.org/">OpenStreetMap</a>, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-	        maxZoom: 17
-	});
-	basemap.addTo(map);
-        /*document.getElementsByClassName("leaflet-map-pane")[0].style.transform ="translate3d(302px, -4px, 0px)";*/
-        
-        /*style="transform: translate3d(302px, -4px, 0px);"*/
-}
-
-function setViewMap(longDeg, longMin, longDir, latDeg, latMin, latDir) {
-	var zoom = 10;
-	var coordonnes = degresMinutesToDecimal(longDeg, longMin, longDir, latDeg, latMin, latDir);
-	map.setView(coordonnes, zoom);
-}
-
-function addMarqeur(nom, url, longDeg, longMin, longDir, latDeg, latMin, latDir) {
-	var coordonnes = degresMinutesToDecimal(longDeg, longMin, longDir, latDeg, latMin, latDir);
-	var marker = L.marker(coordonnes).addTo(map);
-
-	var pageUrl = window.location.href.split('?')[0].split('/');
-	pageUrl.pop();
-	pageUrl = pageUrl.join('/');
-	var capitalUrl = pageUrl + '/capital.html?capitalUrl=' + url;
-	marker.bindPopup('<a href="'+capitalUrl+'">'+nom+'</a>');
-}
-
-function degresMinutesToDecimal(longDeg, longMin, longDir, latDeg, latMin, latDir) {
-	var long = parseInt(longDeg, 10) + parseInt(longMin, 10)/60;
-	var lat = parseInt(latDeg, 10) + parseInt(latMin, 10)/60;
-	if(longDir == 'W') long *= -1;
-	if(latDir == 'S') lat *= -1;
-	return [lat, long];
 }
 
